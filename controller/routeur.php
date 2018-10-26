6d700786b1e7c262f82d6fc4ff92ce2492163053
Titouan Minier-Mancini <titouan.minier-mancini@etu.umontpellier.fr>
2018-10-26 15:55:00 +0200

    Utilisateur & Routeur

---
+++ controller/routeur.php
@@ -0,0 +1,9 @@
+<?php
+require (File::build_path(array("controller","ControllerProduit.php")));
+if(isset($_GET['action'])) {
+    $action = $_GET['action'];  
+}
+else { $action="readAll"; }
+
+ControllerProduit::$action(); 
+?>
