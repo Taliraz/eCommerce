c667374007d70353d8d900aa36c7dc3239df7cb8
Sullyvan Armangau <s.armangau@laposte.net>
2018-10-26 16:31:03 +0200

    update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-require_once(__DIR__. DIRECTORY_SEPARATOR . join(DIRECTORY_SEPARATOR, array("lib","File.php"));
+require_once(__DIR__. DIRECTORY_SEPARATOR . join(DIRECTORY_SEPARATOR, array("lib","File.php")));
 require_once (File::build_path(array("controller","routeur.php")));
 ?>
