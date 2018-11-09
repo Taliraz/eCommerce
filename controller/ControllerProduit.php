41f05ff930de15918a6519c87cf8bbcaccadc459
Sullyvan Armangau <s.armangau@laposte.net>
2018-11-09 14:22:24 +0100

    update ControllerProduit

--- controller/ControllerProduit.php
+++ controller/ControllerProduit.php
@@ -34,7 +34,26 @@ class ControllerProduit {
         require(File::build_path(array("view","view.php")));
     }
 
-   
+   public static function created(){
+      $ModelProduit=new ModelProduit($_POST['nom'],$_POST['prix'],$_POST['origine'],$_POST['poid'],$_POST['couleur'],$_POST['pays'],$_POST['image']);
+      $ModelProduit->save();
+      $controller='produit';
+      $view='created';
+      $pagetitle='Produit créé';
+      require(File::build_path(array("view","view.php")));
+    }
+
+    public static function delete(){
+        $id=$_GET ['id'];
+        ModelProduit::deleteById($id);
+        $tab_p=ModelProduit::getAllProduits();
+        $controller='produit';
+        $view='deleted';
+        $pagetitle='produit supprimé';
+        require(File::build_path(array("view","view.php")));
+    }
+
+
 	
 }
 ?>
