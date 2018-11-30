559309183b0063ca4dd837afff04c0820c183aef
TitouanCao <titouan.minier.mancini@gmail.com>
2018-11-30 15:16:43 +0100

    css

--- view/view.php
+++ view/view.php
@@ -9,12 +9,20 @@
         <header>
             <ul id="menu">
                 <a href="index.php"><li class="menu_button">Accueil</li></a>
-                <a href="index.php?controller=Produit&action=readAll"><li class="menu_button">Produits</li></a>
+                <a href="index.php?controller=Produit&action=readAll">
+                    <li class="menu_button"><span>Produits</span>
+                        <ul class="sub_menu">    
+                            <a href="#"><li>Liste</li></a>
+                            <a href="#"><li>Autre chose</li></a>
+                            <a href="#"><li>Paul l'a dit</li></a>
+                        </ul>
+                    </li>
+                </a>
                 <?php if(Session::is_admin()) {
                     echo '<li class="menu_button">Modération</li>';
                 } 
                 else if (isset($_SESSION['loginUtilisateur']) && Session::is_user($_SESSION['loginUtilisateur'])) {
-                    echo '<li class="menu_button">Mon Profil</li>';
+                    echo '<li class="menu_button">Mon Panier</li>';
                 }
                 else {
                     echo '<a href="index.php?controller=Utilisateur&action=connect"><li class="menu_button" id="menu_button_connect">Connexion</li></a>';
