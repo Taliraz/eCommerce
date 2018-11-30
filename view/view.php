5d37237bab0bb75ad3675221046823aa5eb3ceb2
TitouanCao <titouan.minier.mancini@gmail.com>
2018-11-30 14:45:31 +0100

    Menu

--- view/view.php
+++ view/view.php
@@ -5,18 +5,26 @@
         <title><?php echo $pagetitle; ?></title>
         <?php echo '<link rel="stylesheet" type="text/css" href="'.File::build_path_css(array("view", "view.css")).'">' ?>
     </head>
-    <header>
-       <?php 
-       if (isset($_SESSION['loginUtilisateur'])){
-            echo '<p><a href="index.php?controller=utilisateur&action=read&loginUtilisateur='.$_SESSION['loginUtilisateur'].'">'.$_SESSION['loginUtilisateur'].'</a>
-             <a href="index.php?controller=utilisateur&action=disconnect">Déconnexion</a></p>';
-       }
-       else{
-            echo '<a href="index.php?action=connect&controller=utilisateur">Connexion </a>';
-        }
-        ?>
-    </header>
     <body>
+        <header>
+            <ul id="menu">
+                <a href="index.php"><li class="menu_button">Accueil</li></a>
+                <a href="index.php?controller=Produit&action=readAll"><li class="menu_button">Produits</li></a>
+                <?php if(Session::is_admin()) {
+                    echo '<li class="menu_button">Modération</li>';
+                } 
+                else if (isset($_SESSION['loginUtilisateur']) && Session::is_user($_SESSION['loginUtilisateur'])) {
+                    echo '<li class="menu_button">Mon Profil</li>';
+                }
+                else {
+                    echo '<a href="index.php?controller=Utilisateur&action=connect"><li class="menu_button" id="menu_button_connect">Connexion</li></a>';
+                }
+
+                ?>
+                
+            </ul>
+        </header>
+   
         <?php
             $filepath = File::build_path(array("view", $controller, "$view.php"));
             require $filepath;
@@ -24,3 +32,6 @@
         </div>
     </body>
 </html>
+
+
+
