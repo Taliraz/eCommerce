4f5f8ddce0a016925ab47f2abf6b32e43ca1572f
Sullyvan Armangau <43641531+Taliraz@users.noreply.github.com>
2018-12-09 15:59:09 +0100

    Update Model.php

--- model/Model.php
+++ model/Model.php
@@ -10,8 +10,7 @@ class Model{
 		$login=Conf::getLogin();
 		$password=conf::getPassword();
 		try{
-  			self::$pdo=new PDO("mysql:host=$hostname;dbname=$database_name",$login,$password);
-  			array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+  			self::$pdo=new PDO("mysql:host=$hostname;dbname=$database_name;charset=UTF8",$login,$password);
    
 			self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} 
